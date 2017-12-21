f0a333667ccc43d37d12b8b435b6b2d308793804
bian <bian@test>
2017-12-21 17:56:01 +0800

    add response from gogs

---
+++ gogs_response_example.py
@@ -0,0 +1,76 @@
+# content of  request.json, is dict mode
+# e.g. you can get sender: request.json.get("sender")
+
+request_json = \
+{
+    u'sender': {
+        u'username': u'bianyp',
+        u'email': u'bianyunpeng@ehousechina.com',
+        u'avatar_url': u'https: //secure.gravatar.com/avatar/89025bf194ee41fdd8b0f9ea747f9ec9',
+        u'full_name': u'',
+        u'login': u'bianyp',
+        u'id': 1
+    },
+    u'repository': {
+        u'website': u'',
+        u'forks_count': 0,
+        u'updated_at': u'2017-11-29T08: 16: 14Z',
+        u'private': False,
+        u'full_name': u'bianyp/playbook-test',
+        u'mirror': False,
+        u'owner': {
+            u'username': u'bianyp',
+            u'email': u'bianyunpeng@ehousechina.com',
+            u'avatar_url': u'https: //secure.gravatar.com/avatar/89025bf194ee41fdd8b0f9ea747f9ec9',
+            u'full_name': u'',
+            u'login': u'bianyp',
+            u'id': 1
+        },
+        u'id': 1,
+        u'size': 1381376,
+        u'clone_url': u'http: //10.99.70.38: 3000/bianyp/playbook-test.git',
+        u'watchers_count': 0,
+        u'stars_count': 0,
+        u'empty': False,
+        u'fork': False,
+        u'description': u'',
+        u'parent': None,
+        u'default_branch': u'master',
+        u'html_url': u'http: //10.99.70.38: 3000/bianyp/playbook-test',
+        u'ssh_url': u'ssh: //git@10.99.70.38: 8022/bianyp/playbook-test.git',
+        u'open_issues_count': 0,
+        u'name': u'playbook-test',
+        u'created_at': u'2017-09-06T03: 58: 15Z'
+    },
+    u'pusher': {
+        u'username': u'bianyp',
+        u'email': u'bianyunpeng@ehousechina.com',
+        u'avatar_url': u'https: //secure.gravatar.com/avatar/89025bf194ee41fdd8b0f9ea747f9ec9',
+        u'full_name': u'',
+        u'login': u'bianyp',
+        u'id': 1
+    },
+    u'commits': [{
+        u'committer': {
+            u'username': u'',
+            u'name': u'bian',
+            u'email': u'bian@test'
+        },
+        u'added': [],
+        u'author': {
+            u'username': u'',
+            u'name': u'bian',
+            u'email': u'bian@test'
+        },
+        u'url': u'http: //10.99.70.38: 3000/bianyp/playbook-test/commit/1a43c6d7e2a643f09ae53de6c6a1ba4a973b3276',
+        u'timestamp': u'0001-01-01T00: 00: 00Z',
+        u'modified': [u'publish/scripts/ejucloud/pretest/update.sh'],
+        u'message': u'modifyalittle\n',
+        u'removed': [],
+        u'id': u'1a43c6d7e2a643f09ae53de6c6a1ba4a973b3276'
+    }],
+    u'after': u'1a43c6d7e2a643f09ae53de6c6a1ba4a973b3276',
+    u'compare_url': u'',
+    u'ref': u'refs/heads/master',
+    u'before': u'1a43c6d7e2a643f09ae53de6c6a1ba4a973b3276'
+}
